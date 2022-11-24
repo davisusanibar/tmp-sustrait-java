978fc0284a11ac28d9321a1ba2ea95a4c24d53f5
daviddalisusanibararce <david.dali.susanibar.arce@gmail.com>
2022-11-23 20:10:56 -0500

    fix: sign

--- ci/release/sanity_check.sh
+++ ci/release/sanity_check.sh
@@ -3,18 +3,21 @@
 
 set -euo pipefail
 
-echo "Validate Sonatype OSSRH Credentials."
+echo "START: Validate Sonatype OSSRH Credentials."
 CODE=$(curl -u "$SONATYPE_USER:$SONATYPE_PASSWORD" -sSL -w '%{http_code}' -o /dev/null https://s01.oss.sonatype.org/service/local/staging/profiles)
 if [[ "$CODE" =~ ^2 ]]; then
     echo "Sonatype OSSRH Credentials configured successfully."
 else
     echo "Error to get the profile. Server returned HTTP code $CODE."
 fi
+echo "END: Validate Sonatype OSSRH Credentials."
 
-echo "Validate Signing Private/Public Key."
-echi "Import private key"
+
+echo "START: Validate Signing Private/Public Key."
+echo "Import private key."
 echo $SIGNING_KEY | base64 --decode | gpg  --import
-echo "List secrets key imported"
+echo "List secrets key imported."
 gpg --list-secret-keys $SIGNING_KEY_ID
-echo "Validate passphrase"
+echo "Validate passphrase."
 echo "dummy_value" | gpg -q --batch --status-fd 1 --sign --local-user $SIGNING_KEY_ID --passphrase-fd 0 > /dev/null
+echo "END: Validate Signing Private/Public Key."
