a377c3c027ee6ee57eb4453df4331d629fb4bc83
david dali susanibar arce <davi.sarces@gmail.com>
2022-11-23 19:53:00 -0500

    ci: sanity check environment

---
+++ ci/release/sanity_check.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# shellcheck shell=bash
+
+set -euo pipefail
+
+echo "Validate Sonatype OSSRH Credentials."
+CODE=$(curl -u "$SONATYPE_USER:$SONATYPE_PASSWORD" -sSL -w '%{http_code}' -o /dev/null https://s01.oss.sonatype.org/service/local/staging/profiles)
+if [[ "$CODE" =~ ^2 ]]; then
+    echo "Sonatype OSSRH Credentials configured successfully."
+else
+    echo "Error to get the profile. Server returned HTTP code $CODE."
+fi
+
+echo "Validate Signing Private/Public Key."
+gpg --list-secret-keys
+#echo $SIGNING_KEY | base64 --decode | gpg  --import
+#gpg --list-secret-keys
+echo "dummy_value" | gpg -q --batch --status-fd 1 --sign --local-user $SIGNING_KEY_ID --passphrase-fd 0 > /dev/null
