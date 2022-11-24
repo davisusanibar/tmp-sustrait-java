117266b3a355743f170b75b08cab5f5603e6edf6
daviddalisusanibararce <david.dali.susanibar.arce@gmail.com>
2022-11-24 08:41:30 -0500

    ci: tset

--- ci/release/sanity_check.sh
+++ ci/release/sanity_check.sh
@@ -2,14 +2,14 @@
 # shellcheck shell=bash
 
 set -euo pipefail
-export GPG_TTY=$(tty)
+#export GPG_TTY=$(tty)
 
 echo "START: Validate Sonatype OSSRH Credentials."
 CODE=$(curl -u "$SONATYPE_USER:$SONATYPE_PASSWORD" -sSL -w '%{http_code}' -o /dev/null https://s01.oss.sonatype.org/service/local/staging/profiles)
 if [[ "$CODE" =~ ^2 ]]; then
     echo "Sonatype OSSRH Credentials configured successfully."
 else
-    echo "Error to get the profile. Server returned HTTP code $CODE."
+    echo "Error to validate Sonatype OSSRH Credentials. Server returned HTTP code $CODE."
 fi
 echo "END: Validate Sonatype OSSRH Credentials."
 
@@ -23,7 +23,12 @@ echo "allow-preset-passphrase"  >> ~/.gnupg/gpg-agent.conf
 echo "Reload GPG agent."
 gpgconf --reload gpg-agent
 echo "Preset passphrase on cache."
-"$(gpgconf --list-dirs libexecdir)/gpg-preset-passphrase" -c $KEYGRIP <<< "asa"
+"$(gpgconf --list-dirs libexecdir)/gpg-preset-passphrase" -c $KEYGRIP <<< $SIGNING_PASSWORD
 echo "Test passphrase."
 echo "1234" | gpg -q --batch --status-fd 1 --sign --local-user $SIGNING_KEY_ID --passphrase-fd 0 > /dev/null
+if [ $? = 0 ]; then
+  echo "Public/Private Key Credentials configured successfully."
+else
+  echo "Error to validate Public/Private Key Credentials."
+fi
 echo "END: Validate Signing Private/Public Key."
