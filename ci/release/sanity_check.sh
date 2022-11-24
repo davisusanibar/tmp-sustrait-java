fe1acf7a71092b4f7ef83cfc197c85bd9ac94d25
daviddalisusanibararce <david.dali.susanibar.arce@gmail.com>
2022-11-24 08:55:40 -0500

    ci: commit

--- ci/release/sanity_check.sh
+++ ci/release/sanity_check.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 export GPG_TTY=$(tty)
 
 echo "Validate Sonatype OSSRH Credentials."
-CODE=$(curl -u "$SONATYPE_USER:SONATYPE_PASSWORD" -sSL -w '%{http_code}' -o /dev/null https://s01.oss.sonatype.org/service/local/staging/profiles)
+CODE=$(curl -u "$SONATYPE_USER:$SONATYPE_PASSWORD" -sSL -w '%{http_code}' -o /dev/null https://s01.oss.sonatype.org/service/local/staging/profiles)
 if [[ "$CODE" =~ ^2 ]]; then
     echo "Sonatype OSSRH Credentials configured successfully."
 else
@@ -17,7 +17,7 @@ echo $SIGNING_KEY | base64 --decode | gpg --batch --import
 KEYGRIP=`gpg --with-keygrip --list-secret-keys $SIGNING_KEY_ID | sed -e '/^ *Keygrip  *=  */!d;s///;q'`
 echo "allow-preset-passphrase"  >> ~/.gnupg/gpg-agent.conf
 gpgconf --reload gpg-agent
-"$(gpgconf --list-dirs libexecdir)/gpg-preset-passphrase" -c $KEYGRIP <<< SIGNING_PASSWORD
+"$(gpgconf --list-dirs libexecdir)/gpg-preset-passphrase" -c $KEYGRIP <<< $SIGNING_PASSWORD
 echo "test_use_passphrase_from_cache" | gpg -q --batch --status-fd 1 --sign --local-user $SIGNING_KEY_ID --passphrase-fd 0 > /dev/null
 if [ $? = 0 ]; then
   echo "Public/Private Key Credentials configured successfully."
